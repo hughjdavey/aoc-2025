2d1f916745bb4996468a110bcc858a6b4500e8e0
hughjdavey <9026053+hughjdavey@users.noreply.github.com>
2025-12-01 20:25:52 +0000

    Day 1
    Add inputs, solutions and tests for day 1
    Change README and project name from those of the template project
    Add puzzle inputs to .gitignore
    Update dependencies

--- src/main/kotlin/days/Day1.kt
+++ src/main/kotlin/days/Day1.kt
@@ -2,16 +2,34 @@ package days
 
 class Day1 : Day(1) {
 
+    private val rotations = inputList.map { it[0] to it.substring(1).toInt() }
+    private val safeAfterRotations = Safe().apply { rotations.forEach { rotate(it.first, it.second) } }
+
     override fun partOne(): Any {
-        return inputList.take(2)
-            .map { it.uppercase() }
-            .joinToString(" ")
+        return safeAfterRotations.rotationZeroes
     }
 
     override fun partTwo(): Any {
-        return inputString.split("\n")
-            .filterNot { it.isEmpty() }
-            .map { it.uppercase() }
-            .last()
+        return safeAfterRotations.turnZeroes
+    }
+
+    class Safe(private var dial: Int = 50, private val max: Int = 99, private val min: Int = 0) {
+
+        var rotationZeroes = 0
+        var turnZeroes = 0
+
+        fun rotate(direction: Char, amount: Int) {
+            (0 until amount).forEach { _ -> turn(direction) }
+            if (dial == 0) rotationZeroes++
+        }
+
+        fun turn(direction: Char) {
+            when (direction) {
+                'R' -> if (dial == max) dial = min else dial += 1
+                'L' -> if (dial == min) dial = max else dial -= 1
+                else -> throw IllegalArgumentException("Invalid direction: $direction")
+            }
+            if (dial == 0) turnZeroes++
+        }
     }
 }
